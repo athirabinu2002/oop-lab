ba1ba8052431d8977ff753b43d575df0e35b5202
athirabinu2002 <98386549+athirabinu2002@users.noreply.github.com>
2022-01-25 14:53:28 +0530

    Create sum.java

---
+++ cycle1/sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ class sum
+{
+    public static void main(String args[])
+{
+    System.out.println("Enter two numbers");
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=a+b;
+    System.out.println("sum is"+c);
+}
+}
